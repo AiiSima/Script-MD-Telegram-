6f7bca2c43ad4e75953a2b823ebb065a40011e6b
AiiSima <242970203+AiiSima@users.noreply.github.com>
2026-01-25 18:21:09 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    botToken: "8514256236:AAHfRJIX56XRg8GUkSzqkrtDcrYMuyQYQew", 
+    ownerId: "1160418392", 
+    openweatherApiKey: "",
+    enableStickerMaker: true,
+    enableEconomy: true,
+    enableGames: true,
+    maxGroups: 1000,
+    adminOnlyBroadcast: false
+};
